dd93ad1e3ceda5be0a060ea7d2d11c274d39f3d1
Cameron Matson <cmatson@smu.edu>
2016-11-11 17:11:28 -0600

    chanaged scrape to get all of the debates

--- scrape.py
+++ scrape.py
@@ -1,20 +1,29 @@
 from lxml import html
 import requests
 
-URL = "https://www.washingtonpost.com/news/the-fix/wp/2016/09/26/the-first-trump-clinton-presidential-debate-transcript-annotated/"
+URL = ["https://www.washingtonpost.com/news/the-fix/wp/2016/09/26/the-first-trump-clinton-presidential-debate-transcript-annotated/",
+        "https://www.washingtonpost.com/news/the-fix/wp/2016/10/09/everything-that-was-said-at-the-second-donald-trump-vs-hillary-clinton-debate-highlighted/",
+        "https://www.washingtonpost.com/news/the-fix/wp/2016/10/19/the-final-trump-clinton-debate-transcript-annotated/"]
 def getData():
-    print('scraping \"%s\"' % URL)
-    page = requests.get(URL)
-    tree = html.fromstring(page.content)
+    num = 1
+    for url in URL:
+        print('-scraping \"%s\"' % url)
+        page = requests.get(url)
+        tree = html.fromstring(page.content)
 
-    body = tree.xpath('//article/p/text()')
+        body = tree.xpath("//article/p/text()")
 
-    f = open('./transcripts/transcript_1.txt', 'w');
 
-    for p in body:
-        f.write(p)
-        f.write("\n")
+        path = "debate_"+str(num)
+        f = open('./transcripts/'+path, 'w');
 
-    f.close()
+        for p in body:
+            line = p+"\n"
+            f.write(line)
 
+        f.close()
+
+        num = num+1
     return
+
+getData()
