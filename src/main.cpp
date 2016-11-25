5f25fcf11b5601ca6976c9f8dd8f2fa924ae717f
Cameron Matson <cmatson@smu.edu>
2016-11-25 17:36:27 -0600

    split speakers function

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-
 #include <map>
-
-std::string lower(std::string string) {
-  std::locale loc;
-
-  for (std::string::size_type i = 0; i < string.length(); ++i) {
-    string[i] = std::tolower(string[i],loc);
-  }
-
-  return string;
-}
-
-void writeOut(std::map<std::string, int> words, std::ofstream& ofile) {
-  ofile<<"word,freq\n";
-  for (auto it = words.begin(); it != words.end(); ++it) {
-    ofile<<it->first<<","<<it->second<<"\n";
-  }
-}
 
 void makeStopWords(std::string SWFile, std::vector<std::string> &list) {
   std::ifstream file(SWFile);
@@ -31,6 +13,24 @@ void makeStopWords(std::string SWFile, std::vector<std::string> &list) {
   }
 }
 
+std::string lower(std::string string) {
+  std::locale loc;
+
+  for (std::string::size_type i = 0; i < string.length(); ++i) {
+    string[i] = std::tolower(string[i],loc);
+  }
+
+  return string;
+}
+
+std::string processWord(std::string word) {
+  word = lower(word);
+  std::string text;
+  std::remove_copy_if(word.begin(), word.end(), std::back_inserter(text),std::ptr_fun<int,int>(&std::ispunct));
+  return text;
+
+}
+
 bool isStopWord(std::string word, std::vector<std::string>& stopwords) {
   for (auto it = stopwords.begin(); it != stopwords.end(); ++it) {
     if (*it == word) return true;
@@ -38,44 +38,85 @@ bool isStopWord(std::string word, std::vector<std::string>& stopwords) {
   return false;
 }
 
-std::vector<std::string> sw;
-
-int main(){
-  std::ifstream file("transcripts/debate_1");
-  std::ofstream ofile("data/debate_1.csv");
-
-  std::map< std::string, int > words;
-
-  makeStopWords("misc/stopwords", sw);
-
+void readFrequencies(std::ifstream& file, std::map<std::string, int>& words, std::vector<std::string> sw) {
   if (file) {
     std::string word;
-    int count = 0;
     while ( file >> word ) {
       //to lower case and remove punctuation
-      word = lower(word);
-      std::string text;
-      std::remove_copy_if(word.begin(), word.end(), std::back_inserter(text),std::ptr_fun<int,int>(&std::ispunct));
-      word = text;
+      word = processWord(word);
 
       //check if stopword or a number or blank
-      if (isStopWord(word,sw) || isdigit(word[0]) || word.length() == 0) {
-        std::cout<<word<<std::endl;
-        continue;
-      }
-
-      count++;
-
+      if (isStopWord(word,sw) || isdigit(word[0]) || word.length() == 0) {continue;}
 
       //checks if its in the map if not it adds it with val = 0
-      if ( words[word] == 0 ){};
+      if ( words[word] == 0 ){}
       //increments val (new word currently has val of 0)
       words[word]++;
     }
-    std::cout<<count<<std::endl;
-    std::cout<<words.size()<<std::endl;
-    std::cout<<words.begin()->first<<std::endl;
-    writeOut(words, ofile);
   }
+}
+
+void writeOut(std::map<std::string, int> words, std::ofstream& ofile) {
+  ofile<<"word,freq\n";
+  for (auto it = words.begin(); it != words.end(); ++it) {
+    ofile<<it->first<<","<<it->second<<"\n";
+  }
+}
+
+void splitSpeakers(std::ifstream& file, std::vector<std::string>& clinton,
+                                        std::vector<std::string>& trump,
+                                        std::vector<std::string>& other) {
+  if (file) {
+    std::string speaker;
+    std::string word;
+    file>>word;//this is bad
+    while (file >> word) {
+      if (word == "TRUMP:") {
+        trump.push_back(word);
+        speaker = word;
+        continue;
+      }
+      else if (word == "CLINTON:") {
+        clinton.push_back(word);
+        speaker = word;
+        continue;
+      }
+      else if (word == "HOLT:") {
+        other.push_back(word);
+        speaker = word;
+        continue;
+      }
+
+      word = processWord(word);
+
+      if (speaker == "TRUMP:") {trump.push_back(word);}
+      else if (speaker == "CLINTON:") {clinton.push_back(word);}
+      else {other.push_back(word);}
+    }
+  }
+}
+
+std::vector<std::string> sw;
+
+int main(){
+  std::ifstream file("transcripts/debate_3");
+  std::ofstream ofile("data/debate_3.csv");
+
+  std::map< std::string, int > words;
+
+  std::vector<std::string> trump;
+  std::vector<std::string> clinton;
+  std::vector<std::string> other;
+
+  makeStopWords("misc/stopwords", sw);
+
+  // splitSpeakers(file, clinton, trump, other);
+  // for (auto it = other.begin(); it != other.end(); ++it){
+  //   std::cout<<*it<<std::endl;
+  // }
+
+  readFrequencies(file, words, sw);
+
+  writeOut(words, ofile);
 
 }
